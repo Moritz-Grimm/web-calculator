dd2067fd7c014efd00bffe97e2d27db8ede11454
Moritz-Grimm <molumo@icloud.com>
2025-09-03 13:23:20 +0200

    feat: add decimal point input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-document.getElementById('buttons').addEventListener('click', function(event) {
-  const button = event.target;
+document.getElementById('buttons').addEventListener('click', function (event) {
+  const button = event.target.closest('button');
   const display = document.getElementById('display');
+
 
   if (button.classList.contains('number')) {
     const number = button.getAttribute('data-number');
@@ -12,6 +13,8 @@ document.getElementById('buttons').addEventListener('click', function(event) {
     display.value = '';
   } else if (button.id === 'delete-last') {
     display.value = display.value.slice(0, -1);
+  } else if (button.id === 'dot-button') {
+    display.value += button.getAttribute('data-value');
   } else if (button.id === 'result-button') {
     try {
       display.value = eval(display.value)
