8142cad404aed10e28667c229052f0e1a82aee8d
Moritz Grimm <molumo@icloud.com>
2025-09-15 11:08:57 +0200

    Refactor/css styling (#3)
    * initial refactor
    
    * refactor style.css by adding variables and improved whitemode

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
+const decimalPlaces = document.getElementById('decimal-places');
 
 document.addEventListener('keydown', (event) => {
-  if (/[0-9+\-*/.]/.test(event.key)) {
-    event.preventDefault();
-    display.value += event.key;
-  }
-  if (event.key === "Enter") {
-    event.preventDefault();
-    calculateResult();
-  }
-  if (event.key === "Backspace") {
-    event.preventDefault();
-    backspace();
+  if (!event.ctrlKey && !event.altKey && !event.metaKey) {
+    const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','.',','];
+
+    if (event.key.contains(allowedKeys)) {
+      event.preventDefault();
+      display.value += event.key;
+    }
+    if (event.key === "Enter") {
+      event.preventDefault();
+      calculateResult();
+    }
+    if (event.key === "Backspace") {
+      event.preventDefault();
+      backspace();
+    }
   }
 })
 
 
-document.getElementById('buttons').addEventListener('click', function(event) {
+document.getElementById('buttons').addEventListener('click', function (event) {
   const button = event.target.closest('button');
+
+  if (!button) {
+    return;
+  }
 
   if (button.classList.contains('number')) {
     const number = button.getAttribute('data-number');
@@ -39,7 +48,13 @@ document.getElementById('buttons').addEventListener('click', function(event) {
 
 function calculateResult() {
   try {
-    display.value = eval(display.value)
+    let result = eval(display.value);
+    let decimals = Number(decimalPlaces.value);
+    console.log(decimals);
+    if (result) {
+      result = parseFloat(result.toFixed(decimals));
+      display.value = result;
+    }
   } catch (error) {
     display.value = 'Error';
     console.error(`Error: ${error}`);
