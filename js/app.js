14459c039ad4a0418eae5fbb3934e99804942cbf
Moritz Grimm <molumo@icloud.com>
2025-09-16 07:37:10 +0200

    merge dev into main (#7)
    * refactor/css styling
    
    * improved whitemode
    
    * add shortcuts for delete, clear and result
    
    * add parenthese support
    
    * add flash on result calculation

--- js/app.js
+++ js/app.js
@@ -1,45 +1,76 @@
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
+const decimalPlaces = document.getElementById('decimal-places');
 
 document.addEventListener('keydown', (event) => {
-  if (/[0-9+\-*/.]/.test(event.key)) {
-    event.preventDefault();
-    display.value += event.key;
-  }
-  if (event.key === "Enter") {
-    event.preventDefault();
-    calculateResult();
-  }
-  if (event.key === "Backspace") {
-    event.preventDefault();
-    backspace();
+  if (!event.ctrlKey && !event.altKey && !event.metaKey) {
+    const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '.', ',', '(', ')'];
+    
+    if (allowedKeys.includes(event.key)) {
+      event.preventDefault();
+      display.value += event.key;
+    }
+    if (event.key === "Enter") {
+      event.preventDefault();
+      calculateResult();
+    }
+    if (event.key === "Backspace") {
+      event.preventDefault();
+      backspace();
+    }
+    if (event.key === "c") {
+      clearDisplay();
+    }
+    if (event.key === "d") {
+      backspace();
+    }
+    if (event.key === "r") {
+      calculateResult();
+    }
   }
 })
 
 
-document.getElementById('buttons').addEventListener('click', function(event) {
+document.getElementById('buttons').addEventListener('click', function (event) {
   const button = event.target.closest('button');
 
-  if (button.classList.contains('number')) {
-    const number = button.getAttribute('data-number');
-    display.value += number;
-  } else if (button.classList.contains('operator')) {
-    const operator = button.getAttribute('data-operator')
-    display.value += operator;
-  } else if (button.id === 'clear-button') {
-    display.value = '';
-  } else if (button.id === 'delete-last') {
-    backspace();
-  } else if (button.id === 'dot-button') {
-    display.value += button.getAttribute('data-value');
-  } else if (button.id === 'result-button') {
-    calculateResult();
+  if (!button) return;
+
+  switch (true) {
+    case button.classList.contains('number'):
+      display.value += button.getAttribute('data-number')
+      break;
+    case button.classList.contains('operator'):
+      display.value += button.getAttribute('data-operator');  
+      break;
+    case button.classList.contains('parenthese'):
+      display.value += button.getAttribute('data-parenthese');
+      break;
+    case button.id === 'clear-button':
+      clearDisplay();
+      break;
+    case button.id === 'backspace':
+      backspace();
+      break;
+    case button.id === 'dot-button':
+      display.value += button.getAttribute('data-value')
+      break;
+    case button.id === 'result-button':
+      calculateResult();
+      break;
   }
 });
 
 function calculateResult() {
   try {
-    display.value = eval(display.value)
+    let result = eval(display.value);
+    let decimals = Number(decimalPlaces.value);
+    if (result) {
+      display.classList.add('flash');
+      setTimeout(() => display.classList.remove('flash'), 300);
+      result = parseFloat(result.toFixed(decimals));
+      display.value = result;
+    }
   } catch (error) {
     display.value = 'Error';
     console.error(`Error: ${error}`);
@@ -53,4 +84,8 @@ function backspace() {
 function toggleWhitemode() {
   const element = document.documentElement;
   element.classList.toggle("light-mode");
+}
+
+function clearDisplay() {
+  display.value = '';
 }
